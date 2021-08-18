248806485b9dc772e9baaf2da36699181125054d
Shubham Choudhary <shubhamc183@gmail.com>
2021-08-19 00:58:00 +0530

    Add nodemon in dev dependencies

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # learn-node-express-js
 Learning Node.js with Express.js and other powerful librabies
+
+
+### Start the app
+```
+nodemon index.js
+```
--- api/router.js
+++ api/router.js
@@ -3,7 +3,7 @@ const routerV1 = require('./v1/router');
 const routerV2 = require('./v2/router');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send("v1/ or v2/"));
+router.use('/', (req, res) => res.send("v1/ or v2/"));
 router.use('/v1', (req, res, next) => next(), routerV1);
 router.use('/v2', (req, res, next) => next(), routerV2);
 
