b8442f61f6c03586d34629daf3e44872d20e76e9
Shubham Choudhary <shubhamc183@gmail.com>
2021-08-19 13:04:20 +0530

    router Middleware example

--- api/v1/router.js
+++ api/v1/router.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send("Welcome to the v1 API"));
 
+// Middleware for this router
+router.use((req, res, next) => {
+    console.log(`v1 API ${req.url} called`);
+    next();
+});
+
 router.get('/getUsers', (req, res) => {
     req.container.resolve('getUsers').handleRequest(req, res);
 });
