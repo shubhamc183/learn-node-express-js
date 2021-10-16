c4fa2f214166ce272b9ead8a6a178a8a2d5b204e
Shubham Choudhary <shubhamc183@gmail.com>
2021-10-16 17:11:29 +0530

    Serving a html file

--- api/v2/router.js
+++ api/v2/router.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get('/', (req, res) => res.send("Welcome to the v2 API"));
+
+// dirname provides the absolute path of the this file that can 'joined'(os independent)
+// for forming a path to html file
+router.get('/landing', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', '../', 'views', 'v2', 'index.html'));
+});
 
 module.exports = router;
