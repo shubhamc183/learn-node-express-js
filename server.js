e7fab9634884c3b20e54d24375a479d7cc79adfe
Shubham Choudhary <shubhamc183@gmail.com>
2021-10-16 17:17:10 +0530

    404 using html file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyparser = require('body-parser');
+const path = require('path');
 const apiRouter = require("./api/router");
 
 const server = (container) => {
@@ -22,7 +23,8 @@ const server = (container) => {
         });
         app.use('/api', apiRouter);
         app.use((req, res, next) => {
-            res.status(404).send('<h1>Page not found!</h1>');
+            res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+            // res.status(404).send('<h1>Page not found!</h1>');
         })
 		app.listen(port, () => {
 	        resolve(app);
